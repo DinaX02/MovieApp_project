755635899f0b0d60a53be9b73590d572e7d08ba4
githubTiagoMarques <al.tiagosoares@gmail.com>
2023-04-23 23:28:09 +0100

    Navbar fixed

--- movieapp/src/components/Navbar.js
+++ movieapp/src/components/Navbar.js
@@ -5,19 +5,22 @@ import { Link } from "react-router-dom";
 export function Nav() {
   return (
     <div className="navbar">
-      <Logo className={"logo"} />
-      <h6 className={"selected"}>Movies</h6>
-      <h6>TV Shows</h6>
-      <div className="login_register">
-        <div className="container">
+        <div className={"navLeft"}>
+            <Logo className={"logo"} />
+            <div className={"navLinks"}>
+                <h6 className={"selected"}>Movies</h6>
+                <h6>TV Shows</h6>
+            </div>
+
+        </div>
+
+      <div className="navRight">
+          {/*<div className="container">
           <input className="input_search" type="text" placeholder="Search..." />
           <div className="search"></div>
-        </div>
-        <Link to={"/login"}>
-          <h6>Log In</h6>
-        </Link>
+        </div>*/}
         <Link to={"/register"}>
-          <button className="btn_register_nav">Register</button>
+          <button className="btn_register_nav">Sign In</button>
         </Link>
       </div>
     </div>
