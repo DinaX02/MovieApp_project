1c158658aff517f208fff1aa9fecbc2c7cb3cf4c
cerkeira <ricardocerqueira0@protonmail.com>
2023-04-28 01:28:54 +0100

    homepage start

--- movieapp/src/pages/Homepage.js
+++ movieapp/src/pages/Homepage.js
@@ -1,9 +1,11 @@
 import '../App.css';
 import {Nav} from '../components/Navbar'
+import {Home} from '../components/Home'
 export function Homepage() {
     return (
       <div className="homepage">
       <Nav/>
+      <Home/>
       </div>
     );
   }
