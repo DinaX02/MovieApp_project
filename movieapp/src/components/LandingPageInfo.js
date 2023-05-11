b72fcd3d9a3f86f43a6f5872f91e87f0de2606a5
cerkeira <ricardocerqueira0@protonmail.com>
2023-05-11 15:56:11 +0100

    landing page done

--- movieapp/src/components/LandingPageInfo.js
+++ movieapp/src/components/LandingPageInfo.js
@@ -5,14 +5,14 @@ import { ReactComponent as Info_add } from "../assets/img_landing_page_add.svg";
 
 export function HeroLandingPageInfo() {
   return (
-    <div className="info_landing_page">
+    <div className="hero_landing_page bg_info">
       <div className="info_img">
       <Info_add className="img_ifo" />
 
       </div>
-      <div className="info_text">
+      <div className="hero_text_40">
       <h2>The Best Movie and TV Shows Tracker</h2>
-      <p className="size_text">Keep track of all your favorite movies and TV shows in one place and never miss a scene again - with our movie and TV show list manager!</p>
+      <p className="spacing_left">Keep track of all your favorite movies and TV shows in one place and never miss a scene again - with our movie and TV show list manager!</p>
 
       
   <Link to={"/addToWatchlist"}><button className="landing_btn space-top_try">Try it Now</button></Link>
@@ -22,3 +22,4 @@ export function HeroLandingPageInfo() {
     </div>
   );
 }
+
